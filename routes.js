6fb5935725f513022ecdb2f7ea985f731a0c06a1
Srish29 <asauravsuman@gmail.com>
2019-07-25 10:34:17 +0530

    command with bat file

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ var express = require('express'),
     exec = require('child-process-promise').exec,
     router = express.Router();
 
+var child_process = require('child_process');
 
 /********** Adding routes ************/
 router.get('/',function(req,res){
@@ -21,21 +22,35 @@ router.get('/notfound',function(req,res){
   res.render(path.join(__dirname+'/html/notfound.html'));
 });
 
-router.get('/run',function(req,res){
-  var strCommand = 'echo hello';
+// depricated for now 
+router.get('/run-orig',function(req,res){
+  var strCommand = '';
   exec(strCommand)
       .then(function (result) {
           var stdout = result.stdout;
           var stderr = result.stderr;
-          // console.log('stdout: ', stdout);
-          // console.log('stderr: ', stderr);
+          console.log('stdout: ', stdout);
+          console.log('stderr: ', stderr);
           res.json({'status':true, 'msg':stdout});
       })
       .catch(function (err) {
-          // console.error('ERROR: ', err);
+          console.error('ERROR: ', err);
           res.json({'status':false, 'err':err});
       });
   });
+
+// working now
+router.get('/run',function(req,res){
+  // var strCommand = 'start cmd /k echo Hello, World!';  // working command
+  // var strCommand = 'bin'+path.sep+'test.bat';  // working with bat file
+  var strCommand = 'bin'+path.sep+'open-chrome.bat';  // working with bat file open chrome
+  child_process.exec(strCommand, function(error, stdout, stderr) {
+  //   console.log(stdout);
+  //   console.log(error);
+  //   console.log(stderr);
+    res.json({'status':true, 'msg':stdout, 'stderr':stderr, 'err':error});
+  });
+});
 
 
 
